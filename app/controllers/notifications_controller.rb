2b02a6dc7e63eb998db03c78a4b18fcfc7218031
KatsuyaKanai <kanaisilvia@yahoo.co.jp>
2023-01-26 10:26:58 +0900

    notificationのindex,edit,delete,showの作成。css調整.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -19,18 +19,44 @@ class NotificationsController < ApplicationController
     end
   end
 
+  def show
+    @notification = Notification.find(params[:id])
+  end
+
+
   def edit
+    @notification = Notification.find(params[:id])
   end
 
   def update
+    @notification = Notification.find(params[:id])
+    if @notification.update(notification_edit_params)
+      flash[:notice] = "お知らせが編集されました"
+      redirect_to notifications_path
+    else
+      flash[:alert] = "お知らせの投稿に失敗しました"
+      render :edit
+    end
   end
 
   def destroy
+    @notification = Notification.find(params[:id])
+    if @notification.destroy
+      flash[:notice] = "お知らせが削除されました"
+      redirect_to notifications_path
+    else
+      flash[:alert] = "お知らせの削除に失敗しました"
+      redirect_to notifications_path
+    end
   end
 
   private
 
   def notification_params
+    params.require(:notification).permit(:employee_id, :title, :content)
+  end
+
+  def notification_edit_params
     params.require(:notification).permit(:employee_id, :title, :content)
   end
   
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :employee
 
-  validates :title, presence: true
+  validates :title, length: { minimum: 1, maximum: 50 }
   validates :content, presence: true
 end
